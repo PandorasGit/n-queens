a28c840876b48417d28d35b45136675a7dad8d46
PandorasGit <pandorar9000@gmail.com>
2024-04-18 14:26:24 -0400

    Add initial methods

--- src/csp_problems/N_Queens.java
+++ src/csp_problems/N_Queens.java
@@ -1,4 +1,21 @@
 package csp_problems;
 
-public class N_Queens CSPProblem<String,Integer>{
+import java.util.List;
+import java.util.Map;
+
+public class N_Queens implements CSPProblem<String,Integer>{
+    @Override
+    public Map<String, Variable<String, Integer>> getAllVariables() {
+        return null;
+    }
+
+    @Override
+    public List<String> getNeighborsOf(String vName) {
+        return null;
+    }
+
+    @Override
+    public List<String> getAssigned() {
+        return null;
+    }
 }
