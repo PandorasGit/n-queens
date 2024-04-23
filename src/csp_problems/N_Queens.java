236a0d2c8f7cf6c6e638cae8a2df23658751241b
PandorasGit <pandorar9000@gmail.com>
2024-04-23 14:23:52 -0400

    Create Constructor

--- src/csp_problems/N_Queens.java
+++ src/csp_problems/N_Queens.java
@@ -2,36 +2,86 @@ package csp_problems;
 
 import java.util.*;
 
-public class N_Queens implements CSPProblem<Integer,Boolean>{
+public class N_Queens implements CSPProblem<String,Boolean>{
 
-    private final Map<Integer, Variable<Integer,Boolean>> allVariables;
+    private final Map<String, Variable<String,Boolean>> allVariables;
 
-    private final Map<Integer, Set<Integer>> neighbors = new HashMap<>();
+    private final Map<String, Set<String>> neighbors = new HashMap<>();
     public N_Queens(Integer board_size) {
         allVariables = getAllVariables();
 
         for (int i=0; i<board_size;i++){
-            Set<Integer> rowNeighbors = new HashSet<>();
+            //rowNeighborSet
+            Set<String> rowNeighbors = new HashSet<>();
+
             for (int j=0; j<board_size;j++){
-                
+                String name = i + String.valueOf(j);
+                rowNeighbors.add(name);
             }
+            for(int j=0; j<board_size; j++){
+                String name = i + String.valueOf(j);
+                neighbors.put(name, new HashSet<>(rowNeighbors));
+            }
+        }
+        for (int j=0; j<board_size; j++) {
+            //build the column neighbor set (all variables in the same column)
+            Set<String> columnNeighbors = new HashSet<>();
+            for (int i=0; i<board_size; i++) {
+                String name = i + String.valueOf(j);
+                columnNeighbors.add(name);
+            }
+            for (int i=0; i<board_size; i++) {
+                String name = i + String.valueOf(j);
+                neighbors.get(name).addAll(columnNeighbors);
+            }
+        }
+        //get diagonal neighbor set (all the variables in the same diagonal)
+        for (int i=0; i<board_size;i++){
+            Set<String> diagonalNeighbors = new HashSet<>();
+            for (int j=0; j<board_size;j++){
+                int k = i+j;
+                if (k < board_size) {
+                    String name = j + String.valueOf(j);
+                    diagonalNeighbors.add(name);
+                }
+            }
+            for(int j=0; j<board_size; j++){
+                String name = j + String.valueOf(j);
+                neighbors.put(name, new HashSet<>(diagonalNeighbors));
+            }
+        }
+        for (int i=0; i<board_size;i++){
+            Set<String> diagonalNeighbors = new HashSet<>();
+            for (int j=board_size; j>0; j--){
+                int k = i+j;
+                if (k < board_size) {
+                    String name = j + String.valueOf(j);
+                    diagonalNeighbors.add(name);
+                }
+            }
+            for(int j=0; j<board_size; j++){
+                String name = j + String.valueOf(j);
+                neighbors.put(name, new HashSet<>(diagonalNeighbors));
+            }
+        }
+        for(Map.Entry<String,Set<String>> e : neighbors.entrySet()){
+            e.getValue().remove(e.getKey());
+            //   System.out.println(e.getValue().size());
         }
     }
 
     @Override
-    public Map<Integer, Variable<Integer, Boolean>> getAllVariables() {
+    public Map<String, Variable<String, Boolean>> getAllVariables() {
         return null;
     }
 
-    @Override
-    public List<Integer> getNeighborsOf(Integer vName) {
-        return null;
+    public List<String> getNeighborsOf(String name){
+        return(new ArrayList<>(neighbors.get(name)));
     }
 
 
-
     @Override
-    public List<Integer> getAssigned() {
+    public List<String> getAssigned() {
         return null;
     }
 }
